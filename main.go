cbd7ed7b35fc02ff32099ee7ceae3114f0d69926
ggorockee <ggorockee@gmail.com>
2024-11-22 11:41:03 +0900

    fiber update

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+
+	app.Get("/v1/helloworld", func(c *fiber.Ctx) error {
+		return c.Status(http.StatusOK).JSON(fiber.Map{
+			"status": "ok",
+			"error":  false,
+		})
+	})
+
+	app.Listen(":3000")
 }
