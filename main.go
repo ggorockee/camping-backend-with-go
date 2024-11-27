3f97d3239b088295ffd61a4cc3f80da7ce35d9ac
ggorockee <ggorockee@gmail.com>
2024-11-27 15:50:20 +0900

    dsn update

--- main.go
+++ main.go
@@ -88,7 +88,7 @@ func databaseConnection() *gorm.DB {
 		)
 	}
 
-	dsn = fmt.Sprintf("%s:@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
